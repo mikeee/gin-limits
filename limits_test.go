d6760adc1dcdcf9b125c69220300c747818d2866
Mike <hey@mike.ee>
2019-04-05 23:38:36 +0100

    implement a basic test case for the limiter

--- limits_test.go
+++ limits_test.go
@@ -1,1 +1,24 @@
 package limits
+
+import (
+	"github.com/stretchr/testify/assert"
+	"time"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+)
+
+// TestLimit expects a specified limit of 1 to allow one request.
+func TestLimit(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+	r := gin.Default()
+	r.Use(limits.MaxConnections(1))
+
+	r.GET("/", func(*gin.Context) {
+		time.Sleep(1 * time.Second)
+	})
+
+	// Making a request
+	w := performRequest(r, "GET", "/")
+	assert.Equal(t, 200, w.Code)
+}
