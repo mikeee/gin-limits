5c218af2162842cee1a08ebe518d0b52a3bb30ea
Mike <hey@mike.ee>
2019-04-06 11:51:00 +0100

    implemented a semaphore based permits system for requests

--- limits.go
+++ limits.go
@@ -1,1 +1,21 @@
 package limits
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+// MaxConnections creates an artificial limit on connections.
+func MaxConnections(count int) gin.HandlerFunc {
+	semaphore := make(chan struct{}, count)
+	acquire := func() {
+		semaphore <- struct{}{}
+	}
+	relase := func() {
+		<-semaphore
+	}
+	return func(context *gin.Context) {
+		acquire()      // before request
+		defer relase() // after request
+		context.Next()
+	}
+}
