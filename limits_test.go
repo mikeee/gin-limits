66ed6cfbef791e711352b19d2653bd559e2deebb
Mike <hey@mike.ee>
2019-04-06 11:42:47 +0100

    refactor structure &  implement time based testing

--- limits_test.go
+++ limits_test.go
@@ -1,24 +1,55 @@
 package limits
 
 import (
-	"github.com/stretchr/testify/assert"
-	"time"
+	"net/http"
+	"net/http/httptest"
+	"sync"
 	"testing"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
 )
 
-// TestLimit expects a specified limit of 1 to allow one request.
+// performRequest has been borrowed from the Gin repo - https://github.com/gin-gonic/gin/blob/master/routes_test.go
+func performRequest(r http.Handler, method, path string) *httptest.ResponseRecorder {
+	req, _ := http.NewRequest(method, path, nil)
+
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, req)
+
+	return w
+}
+
+// TestLimit imposes a requests throttle of 10 per second and against two tests expects the duration to be approximately 1 second given 20 requests.
 func TestLimit(t *testing.T) {
 	gin.SetMode(gin.TestMode)
 	r := gin.Default()
-	r.Use(limits.MaxConnections(1))
+	r.Use(MaxConnections(10))
 
 	r.GET("/", func(*gin.Context) {
-		time.Sleep(1 * time.Second)
+		time.Sleep(time.Second / 2)
 	})
 
-	// Making a request
-	w := performRequest(r, "GET", "/")
-	assert.Equal(t, 200, w.Code)
+	var wg sync.WaitGroup
+	var result = true
+	var test bool
+
+	var starttime = time.Now()
+	for i := 0; i < 20; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			w := performRequest(r, "GET", "/")
+			if w.Code != 200 {
+				result = false
+			}
+		}()
+	}
+	wg.Wait()
+	var timer = time.Since(starttime)
+	if (timer < time.Millisecond*1100) && (timer > time.Microsecond*900) && result == true {
+		test = true
+	}
+	assert.Equal(t, true, test)
 }
